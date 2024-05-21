85ad99aac42c858d66814d106aefeaccb0d800b7
Mutawakil Hatem <mutawakkilhatem@gmail.com>
2024-05-21 16:18:12 +0200

    add heder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Header from './components/header/Header'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    
+    <Header/>
     </>
   )
 }
