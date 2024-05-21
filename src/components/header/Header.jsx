0a01c1dd6713f68e91399d84764a5421f7aadb57
Mutawakil Hatem <mutawakkilhatem@gmail.com>
2024-05-21 16:19:28 +0200

    add header file

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html " className="nav__logo">
+          Mutawakil
+        </a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
