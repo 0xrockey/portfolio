dec4450ecabe91c7fb03abed499cab3f773eb5bd
Rockey Rocket <rocky.rocket.dev@gmail.com>
2024-05-30 11:19:30 +0200

    add home section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
+import "./home.css";
+import Social from "./Social";
+import Data from "./Data";
+import ScrollDwon from "./ScrollDwon";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <section className=" home section" id="home ">
+        <div className="home__container container grid ">
+          <div className="home__content grid ">
+            <Social />
+            <div className="home__img"></div>
+            <Data />
+          </div>
+          <ScrollDwon />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://x.com/?lang=en"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-twitter-alt"></i>
+      </a>
+      <a
+        href="https://github.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
