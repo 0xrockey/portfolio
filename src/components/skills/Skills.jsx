023b0f043f80c1c737c5cd995695bfa81e67f9f5
Rockey Rocket <rocky.rocket.dev@gmail.com>
2024-05-31 07:32:13 +0200

    fix class name

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,7 +7,7 @@ const Skills = () => {
     <section className="skills scection" id="skills">
       <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">My technical level </span>
-      <div className="skills__comtainer grid">
+      <div className="skills__container grid">
         <Frontend />
         <Backend />
       </div>
