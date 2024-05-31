c588b9800e3e965b3e4b2c032f68ac68f27c859c
Rockey Rocket <rocky.rocket.dev@gmail.com>
2024-05-31 06:23:19 +0200

    add main section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <section className="skills scection" id="skills">
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My technical level </span>
+      <div className="skills__comtainer grid"></div>
+    </section>
+  );
 };
 
 export default Skills;
