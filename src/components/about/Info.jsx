3973071adb91752170d3ca71d59651f79b22ada2
Rockey Rocket <rocky.rocket.dev@gmail.com>
2024-05-31 05:09:09 +0200

    add card info

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div className="about__info grid ">
+      <div className="about__box">
+        {/* this for icon */}
+        <i className="bx bx-award about__icon"></i>
+        <h3 className="about__title">Experience </h3>
+        <span className="about__subtitle">4 Years Working </span>
+      </div>
+      <div className="about__box">
+        {/* this for icon */}
+        <i className="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Completed </h3>
+        <span className="about__subtitle">8 + Project</span>
+      </div>
+      <div className="about__box">
+        {/* this for icon */}
+        <i className="bx bx-support about__icon"></i>
+        <h3 className="about__title">Support </h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
