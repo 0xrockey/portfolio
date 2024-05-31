7b0efe07c2ffe890c26d28d4f5316808c44ae351
Rockey Rocket <rocky.rocket.dev@gmail.com>
2024-05-31 06:30:50 +0200

    import front and back

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
+import "./skills.css";
+import Frontend from "./Frontend";
+import Backend from "./Backend";
 const Skills = () => {
   return (
     <section className="skills scection" id="skills">
       <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">My technical level </span>
-      <div className="skills__comtainer grid"></div>
+      <div className="skills__comtainer grid">
+        <Frontend />
+        <Backend />
+      </div>
     </section>
   );
 };
