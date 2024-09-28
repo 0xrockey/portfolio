a609ce39a94c0b554c9e36710652d5d18b1bdce7
0xrockey <rocky.rocket.dev@gmail.com>
2024-09-28 03:58:06 +0200

    .

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -103,7 +103,7 @@ const Contact = () => {
               ></textarea>
             </div>
             <button className="button button--flex">
-              Say Hello
+              Send Message
               <svg
                 class="button__icon"
                 xmlns="http://www.w3.org/2000/svg"
