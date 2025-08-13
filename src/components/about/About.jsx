77b51da921bf99cdbccbb85b1fa4375331450acd
0xrockey <rocky.rocket.dev@gmail.com>
2025-08-13 14:59:37 +0200

    update CV import to use English.pdf

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/English.pdf";
 import Info from "./Info";
 const About = () => {
   return (
