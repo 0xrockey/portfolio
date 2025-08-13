9abea34fb844a18c5456a2d2980cc459c72ceb47
0xrockey <rocky.rocket.dev@gmail.com>
2025-08-13 15:01:21 +0200

    Update social media links in Social component

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,28 +4,28 @@ const Social = () => {
   return (
     <div className="home__social">
       <a
-        href="https://www.instagram.com/"
+        href="https://www.instagram.com/k.oo.la/?__pwa=1"
         className="home__social-icon"
         target="_blank"
       >
         <i class="uil uil-instagram"></i>
       </a>
       <a
-        href="https://x.com/?lang=en"
+        href="https://x.com/404hatch"
         className="home__social-icon"
         target="_blank"
       >
         <i class="uil uil-twitter-alt"></i>
       </a>
       <a
-        href="https://github.com/"
+        href="https://github.com/0xrockey"
         className="home__social-icon"
         target="_blank"
       >
         <i class="uil uil-github-alt"></i>
       </a>
       <a
-        href="https://www.linkedin.com/"
+        href="https://www.linkedin.com/in/mutwakil-hatem-32bab71b2/"
         className="home__social-icon"
         target="_blank"
       >
