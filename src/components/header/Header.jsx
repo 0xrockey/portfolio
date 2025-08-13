2646589fc78cd9a24a68160adcba894ba056bcb2
0xrockey <rocky.rocket.dev@gmail.com>
2025-08-13 15:01:10 +0200

    Comment out portfolio navigation item in Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -69,7 +69,7 @@ function Header() {
                 <i className="uil uil-briefcase-alt  nav__icon"></i> Services
               </a>
             </li>
-            <li className="nav__item">
+            {/*<li className="nav__item">
               <a
                 href="#portfolio"
                 onClick={() => setActiveNav("#portfolio")}
@@ -79,7 +79,7 @@ function Header() {
               >
                 <i className="uil uil-scenery nav__icon"></i> Portfolio
               </a>
-            </li>
+            </li>*/}
             <li className="nav__item">
               <a
                 href="#contact"
